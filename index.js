e4f41a248d95a37c8d1e346afc38c4d56be2eaf3
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-08-23 14:04:44 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-var firstName = "Mukta";
-var lastName = "Bosu";
-var age = 50;
-var isMarried = true;
-var hobbies = [1, 2, 3, 4, 5];
-function view() {
-    console.log("This is a function");
+var number = 42.0;
+var bool = true;
+var num1 = 10.0;
+var result = number + num1;
+console.log(result);
+function add(a, b) {
+    var sum;
+    return sum = a + b;
 }
-var fullname = firstName.concat(" ", lastName);
-console.log("The name is", fullname);
-console.log("The age is", age);
-console.log("Married:", isMarried);
-console.log("Hobbies:", hobbies);
-view();
+console.log(add(20, 30));
