c24d55c95be96deb67418562a4d05293e47e2be4
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-08-23 14:05:25 +0600

    Add files via upload

---
+++ ab_class.js
@@ -0,0 +1,36 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var user = /** @class */ (function () {
+    function user(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return user;
+}());
+var student = /** @class */ (function (_super) {
+    __extends(student, _super);
+    function student(name, age, id) {
+        var _this = _super.call(this, name, age) || this;
+        _this.id = id;
+        return _this;
+    }
+    student.prototype.viewuser = function () {
+        console.log("Name: ".concat(this.name, ",Age: ").concat(this.age, ",ID: ").concat(this.id));
+    };
+    return student;
+}(user));
+var student1 = new student("Mukta Bosu", 50, 41220300439);
+student1.viewuser();
