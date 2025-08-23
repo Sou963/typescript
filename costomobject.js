a8cbebe77ef8c2672aa499af2474af426dd3c537
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-08-23 14:09:53 +0600

    Add files via upload

---
+++ costomobject.js
@@ -0,0 +1,10 @@
+var users = [];
+var user1 = ["mukta bosu", 50];
+users.push(user1);
+console.log(users);
+var getRequest;
+getRequest = "GET";
+function sendRequest(request) {
+    console.log(request);
+}
+sendRequest("GET");
