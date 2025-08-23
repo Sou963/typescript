9240ed89fe14a91aa38bd601e6a60dbc11ef7b8d
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-08-23 14:15:12 +0600

    Add files via upload

---
+++ import.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var export_1 = require("./export");
+function viewinfo() {
+    console.log("Name: ".concat(export_1.name, ", Age: ").concat(export_1.age));
+}
+viewinfo();
